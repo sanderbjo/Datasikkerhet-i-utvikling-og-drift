3ca595faa705c66481ec627d580351b9bfbfb0a0
Marius Bjørnstad Johansen <mariubj@hiof.no>
2024-02-07 10:18:00 +0100

    svar for samme id som melding sann at det bare skal være mulig med et svar pr melding

--- svar.php
+++ svar.php
@@ -4,17 +4,19 @@
 
 <?php
    require "includes/db-connection.php";
-   $sql = "INSERT INTO `svar` (`innhold`, `melding_id`, `bruker_id`) VALUES ('".$_POST["svar"]."', '".$_POST["id"]."', '".$_POST["bruker_id"]."');";
-   
+
+
+   $sql = "INSERT INTO `svar` (`id`,`innhold`, `melding_id`, `bruker_id`) VALUES ('".$_POST["id"]."','".$_POST["svar"]."', '".$_POST["id"]."', '".$_POST["bruker_id"]."');";
 
    if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-  } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-  }
-  
+    echo "Svaret ditt har blitt registert";
 
-  $conn->close();
+   } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+
+   }
+
+   $conn->close();
    ?>
 
 
